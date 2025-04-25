bb26d1c1b2ed721d66ca9d2ce1027837e3cc4638
Gabriel <ggendron@comitari.fr>
2025-04-25 11:00:34 +0200

    Update Extract_Users.ps1

--- Extract_Users.ps1
+++ Extract_Users.ps1
@@ -1,4 +1,4 @@
-Connect-MgGraph -Scopes "User.Read.All"
+Connect-Graph -scopes "User.Read.All", "Group.Read.All"
 
 $users = Get-MgUser -All | Where-Object { $_.AccountEnabled -eq $true }
 
@@ -22,3 +22,35 @@ $export = $users | Select-Object `
 $export | Export-Csv -Path ".\entra_users.csv" -NoTypeInformation -Encoding UTF8
 
 Write-Host "✅ Export here : entra_users.csv"
+
+
+# Get Groups
+$allGroups = Get-MgGroup -All
+
+$results = @()
+
+foreach ($group in $allGroups) {
+    $groupId = $group.Id
+    $groupName = $group.DisplayName
+
+    # Get Groups Members
+    $members = Get-MgGroupMember -GroupId $groupId -All
+
+    foreach ($member in $members) {
+        if ($member.AdditionalProperties.'@odata.type' -eq '#microsoft.graph.user') {
+            $user = Get-MgUser -UserId $member.Id
+            $results += [PSCustomObject]@{
+                Groupe        = $groupName
+                NomUtilisateur = $user.DisplayName
+                Email         = $user.UserPrincipalName
+                IDUtilisateur = $user.Id
+            }
+        }
+    }
+}
+
+# CSV Export
+$results | Export-Csv -Path "groups_and_members.csv" -NoTypeInformation -Encoding UTF8
+
+Write-Host "✅ Export here : groups_and_members.csv"
+
